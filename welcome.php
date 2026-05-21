ead89a70a47716e7b5e1d8ee6c190b1a6d478bcb
106566951 <106566951@student.swin.edu.au>
2026-05-21 14:43:40 +1000

    added all files with changes

---
+++ welcome.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+if (isset($_SESSION['user'])){
+    include 'header.inc';
+    echo "Welcome, ".$_SESSION['user'];
+    include 'footer.inc';
+}else{
+    header('Location: login.php');
+}
+?>
